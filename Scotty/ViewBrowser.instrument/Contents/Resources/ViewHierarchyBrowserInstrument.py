56f8bc7b7838b53bd5519f951aae7694a1776a33
eaganj <eaganj@2173f847-0edd-4382-bdcc-49b71dc1bf57>
2011-08-29 11:44:41 +0000

    More glass window refactoring.
    git-svn-id: https://www.lri.fr/svn/in-situ/code/Instruments/trunk@3957 2173f847-0edd-4382-bdcc-49b71dc1bf57

--- Scotty/ViewBrowser.instrument/Contents/Resources/ViewHierarchyBrowserInstrument.py
+++ Scotty/ViewBrowser.instrument/Contents/Resources/ViewHierarchyBrowserInstrument.py
@@ -31,11 +31,16 @@ class ScottyViewHierarchyBrowserInstrument(Instrument):
     def shouldHijackInteraction(self):
         return False
     
-    def activate(self, context=None):
+    def activate(self, context):
         super(ScottyViewHierarchyBrowserInstrument, self).activate(context)
         if not self._controller:
             self._controller = ViewHierarchyBrowserController.alloc().initWithInstrument_(self)
         self._controller.showWindow_(self)
+        
+        glassWindows = context.attachGlassWindowToAllWindows(interactive=False)
+        for glassWindow in glassWindows:
+            parentWindow = glassWindow.parentWindow()
+            glassWindow.setContentView_(ViewHierarchyBrowserGlassView(parentWindow.frame(), self))
         self.deactivate() # FIXME: this should happen on window close!
                 
     def mouseUp(self, event):
@@ -50,7 +55,8 @@ class ScottyViewHierarchyBrowserInstrument(Instrument):
         window = widget.window()
         instrumentManager = InstrumentManager.sharedInstrumentManager()
         instrumentManager.resetGlassViewsForInstrument_(self)
-        glassView = instrumentManager.glassViewForWindow(widget.window())
+        # glassView = instrumentManager.glassViewForWindow(widget.window())
+        glassView = self.context.glassViewForWindow(widget.window())
         if glassView:
             glassView.highlightWidget_(widget)
     
